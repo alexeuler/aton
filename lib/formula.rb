5dfc10f6ebef0db9fd7af933d9070491427f8e4b
alex <alex@work>
2014-10-17 18:14:15 +0400

    added types

--- lib/formula.rb
+++ lib/formula.rb
@@ -1,3 +1,5 @@
+require_relative 'boiler'
+
 class Formula
 
   INNER_PARENTHESIS_REGEX=/(((\d+,)?\d+)\*)?(-?)(\(.*\))/ #search for 2*(...) or +(...) or -(...)
@@ -11,22 +13,41 @@ class Formula
     exp=exp[2..-1] #Removing #=
     hash = {}
     factor = 1.0
-    expression_to_vector(hash,factor,exp)
+    types = form_types(exp)
+    expression_to_vector(hash,factor,exp, types)
   end
 
-  def expression_to_vector(hash, factor, exp)
+  def form_types(exp)
+    types = []
+    match = /!(O)/.match exp
+    if match
+      types=Boiler::Types
+      types.delete('K') if /!(K)/.match(exp)
+      types.delete('L') if /!(L)/.match(exp)
+      types.delete('M') if /!(M)/.match(exp)
+      types.delete('N') if /!(N)/.match(exp)
+    else
+      types.push('K') if /!(K)/.match(exp)
+      types.push('L') if /!(L)/.match(exp)
+      types.push('M') if /!(M)/.match(exp)
+      types.push('N') if /!(N)/.match(exp)
+    end
+    types.sort
+  end
+
+  def expression_to_vector(hash, factor, exp, types)
     exp=exp[1..-2] if exp[0] == '(' #canonical view, no parenthesis
     return if exp=='' or exp.nil?
     inner_exp_match = INNER_PARENTHESIS_REGEX.match exp
     if inner_exp_match
       post_processing_exp=inner_exp_match[5]
       post_processing_factor = inner_exp_match[2].nil? ? factor : factor * inner_exp_match[2].sub(',','.').to_f
-      expression_to_vector(hash, post_processing_factor, post_processing_exp)
+      expression_to_vector(hash, post_processing_factor, post_processing_exp, types)
       exp = inner_exp_match.pre_match + inner_exp_match.post_match
     end
     lexems = exp.split('+')
     lexems.each do |lexem|
-      update_vector_for_lexem(hash, factor, lexem) unless lexem.empty?
+      update_vector_for_lexem(hash, factor, lexem, types) unless lexem.empty?
     end
     hash
   end
@@ -38,7 +59,7 @@ class Formula
     match.pre_match+'!'+match.post_match
   end
 
-  def update_vector_for_lexem(hash, factor, lexem)
+  def update_vector_for_lexem(hash, factor, lexem, types)
     match = /(((\d+,)?\d+)\*)?(.*)/.match lexem
     if match[1]
       number = match[2].sub(',','.')
@@ -46,9 +67,11 @@ class Formula
     end
     lexem = match[4]
     ref = lexem_to_reference lexem
-    id = @references.get_id(ref)
-    hash[id]||=0
-    hash[id]+=factor
+    types.each do |type|
+      id = @references.get_id(ref) + "&#{type}"
+      hash[id]||=0
+      hash[id]+=factor
+    end
   end
 
 end
--- spec/formula_spec.rb
+++ spec/formula_spec.rb
@@ -4,73 +4,73 @@ require 'formula'
 
 describe Formula do
   it 'Simple formula' do
-    form = '#=Зборка!O95+Зборка!O96+Зборка!O97+Зборка!O98'
+    form = '#=Зборка!K95+Зборка!K96+Зборка!K97+Зборка!K98'
     hash = App::Formula.translate(form)
     expected = {
-        'Vektor2'=> 1,
-        'Vektor3'=> 1,
-        'Vektor4'=> 1,
-        'Vektor5'=> 1,
+        'Vektor2&K'=> 1,
+        'Vektor3&K'=> 1,
+        'Vektor4&K'=> 1,
+        'Vektor5&K'=> 1,
     }
     expect(hash).to eq(expected)
   end
 
   it 'Simple formula with parenthesis' do
-    form = '#=(Зборка!O95+Зборка!O96+Зборка!O97+Зборка!O98)'
+    form = '#=(Зборка!K95+Зборка!K96+Зборка!K97+Зборка!K98)'
     hash = App::Formula.translate(form)
     expected = {
-        'Vektor2'=> 1,
-        'Vektor3'=> 1,
-        'Vektor4'=> 1,
-        'Vektor5'=> 1,
+        'Vektor2&K'=> 1,
+        'Vektor3&K'=> 1,
+        'Vektor4&K'=> 1,
+        'Vektor5&K'=> 1,
     }
     expect(hash).to eq(expected)
   end
 
   it 'Simple formula with factor' do
-    form = '#=3*(Зборка!O95+Зборка!O96+Зборка!O97+Зборка!O98)'
+    form = '#=3*(Зборка!K95+Зборка!K96+Зборка!K97+Зборка!K98)'
     hash = App::Formula.translate(form)
     expected = {
-        'Vektor2'=> 3,
-        'Vektor3'=> 3,
-        'Vektor4'=> 3,
-        'Vektor5'=> 3,
+        'Vektor2&K'=> 3,
+        'Vektor3&K'=> 3,
+        'Vektor4&K'=> 3,
+        'Vektor5&K'=> 3,
     }
     expect(hash).to eq(expected)
   end
 
   it 'Nested simple formula' do
-    form = '#=2*(Зборка!O95+2*Зборка!O96+Зборка!O97+Зборка!O98)'
+    form = '#=2*(Зборка!K95+2*Зборка!K96+Зборка!K97+Зборка!K98)'
     hash = App::Formula.translate(form)
     expected = {
-        'Vektor2'=> 2,
-        'Vektor3'=> 4,
-        'Vektor4'=> 2,
-        'Vektor5'=> 2,
+        'Vektor2&K'=> 2,
+        'Vektor3&K'=> 4,
+        'Vektor4&K'=> 2,
+        'Vektor5&K'=> 2,
     }
     expect(hash).to eq(expected)
   end
 
   it 'Nested simple formula with float' do
-    form = '#=2*(Зборка!O95+2,5*Зборка!O96+Зборка!O97+Зборка!O98)'
+    form = '#=2*(Зборка!K95+2,5*Зборка!K96+Зборка!K97+Зборка!K98)'
     hash = App::Formula.translate(form)
     expected = {
-        'Vektor2'=> 2,
-        'Vektor3'=> 5,
-        'Vektor4'=> 2,
-        'Vektor5'=> 2,
+        'Vektor2&K'=> 2,
+        'Vektor3&K'=> 5,
+        'Vektor4&K'=> 2,
+        'Vektor5&K'=> 2,
     }
     expect(hash).to eq(expected)
   end
 
   it 'Nested formula with float' do
-    form = '#=Зборка!O95+2*(2,5*(Зборка!O96+Зборка!O97)+Зборка!O97+Зборка!O98)'
+    form = '#=Зборка!K95+2*(2,5*(Зборка!K96+Зборка!K97)+Зборка!K97+Зборка!K98)'
     hash = App::Formula.translate(form)
     expected = {
-        'Vektor2'=> 1,
-        'Vektor3'=> 5,
-        'Vektor4'=> 7,
-        'Vektor5'=> 2,
+        'Vektor2&K'=> 1,
+        'Vektor3&K'=> 5,
+        'Vektor4&K'=> 7,
+        'Vektor5&K'=> 2,
     }
     expect(hash).to eq(expected)
   end
